d2af33944d0451968766d1cd9d82bb1cd66fc8c9
razumolla <rajuahmed9344@gmail.com>
2022-05-09 12:17:44 +0600

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,18 @@
+/**
+ * -------------------------
+ * One Time for any computer
+ * --------------------------
+ * 1. Create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ------------------------
+ * For each project one time
+ * -------------------------
+ * 1. heroku create
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -10,6 +11,22 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('Decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 // connect> your application
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tienk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,6 +34,17 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
+        // Auth (jwt)
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // services API
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -31,7 +59,9 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             res.send(service)
         })
+
         // POST : client to server(insert a document)
+
         app.post('/service', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
@@ -47,6 +77,28 @@ async function run() {
             res.send(result);
         })
 
+        // order collection api
+
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
+
+        })
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
     }
     finally {
 
@@ -61,5 +113,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log('Listening to poet', port);
+    console.log('Listening to port', port);
 })
